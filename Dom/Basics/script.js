b9c0cd42c867c651d3a6583f85e98fd9606a0837
Sunny Kumar <140904663+sunnykr7781@users.noreply.github.com>
2023-09-02 20:40:54 +0530

    Add files via upload

--- Dom/Basics/script.js
+++ Dom/Basics/script.js
@@ -1,5 +1,15 @@
 var bulb = document.querySelector("#box");
 var btn = document.querySelector("button");
+var flag = 0;
 btn.addEventListener("click", function () {
-  bulb.style.backgroundColor = "yellow";
+  if (flag == 0) {
+    bulb.style.backgroundColor = "yellow";
+    flag = 1;
+    btn.innerHTML = "OFF";
+   
+  } else {
+    bulb.style.backgroundColor = "transparent";
+    flag = 0;
+    btn.innerHTML = "ON";
+  }
 });
