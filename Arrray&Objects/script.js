bf0ba426f8985fbb062f95a71d7bc8aa987d7f4a
Sunny Kumar <140904663+sunnykr7781@users.noreply.github.com>
2023-09-05 21:07:02 +0530

    Add files via upload

--- Arrray&Objects/script.js
+++ Arrray&Objects/script.js
@@ -1,36 +1,24 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// how to use / access
-arr[0]; // arr[index]
-
-// to remove a value from last in array
-arr.pop(); // now arr = [1,2,3,4,5,6,7,8]
-
-// to add a value to last
-arr.push(10); // now arr = [1,2,3,4,5,6,7,8,10]
-
-// to remove a value from any index
-arr.splice(4, 1); // arr.splice(index, how many value to be removed)
-
-// Objects => when we need to store about one element only we store
-//  it in object in key: value pair in curlybraces({ })
-
-var student = {
-  name: "aditya",
-  rollno: 34,
-  emailid: "aditya11@gmail.com",
-};
-
-// to access it
-student.name; // name of the objectvariable.key
-//anything that comes before a dot in js is a object
-
-var a = {
-  b: {
-    c: {
-      d: 123,
-    },
+var story = [
+  {
+    dp: "",
+    img: "",
   },
-};
-a.b.c.d; // to access the value of d we put . it means we goes inside an
-//  object a. means we ar inside the a thats why we done c.d it means we are
-//   inside c and asking the value of d
+  {
+    dp: "",
+    img: "",
+  },
+  {
+    dp: "",
+    img: "",
+  },
+  {
+    dp: "",
+    img: "",
+  },
+];
+var clutter = " ";
+story.array.forEach((element) => {
+  clutter += `<div class="story">
+  <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="" />
+</div>`;
+});
