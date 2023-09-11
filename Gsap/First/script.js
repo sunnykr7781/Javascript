67dd4c35bedf73a61868ca242bfa2992d7c69949
Sunny Kumar <140904663+sunnykr7781@users.noreply.github.com>
2023-09-12 02:14:24 +0530

    Add files via upload

--- Gsap/First/script.js
+++ Gsap/First/script.js
@@ -1,2 +1,36 @@
 var t = gsap.timeline();
-t.from();
+gsap.set(".z, .first , .second , .img", {
+  opacity: 0,
+  y: 20,
+});
+
+gsap.set(".left #textdwn", {
+  height: 1,
+});
+t.from(".left", {
+  width: 0,
+  duration: 1.5,
+  ease: Expo.easeInOut,
+});
+t.from(".right", {
+  width: 0,
+  duration: 1.5,
+  ease: Expo.easeInOut,
+});
+t.to(".z , .first , .second , .img", {
+  delay: -1.5,
+
+  opacity: 1,
+  ease: Expo.easeInOut,
+
+  y: 0,
+  stagger: 0.15,
+});
+t.to(".right img", {
+  opacity: 1,
+  delay: -2.1,
+  ease: Expo.easeInOut,
+
+  scale: 0.7,
+  duration: 1,
+});
