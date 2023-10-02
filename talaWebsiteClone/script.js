7f8dcf16df83c808ee080993e64d600d58cd82be
Sunny Kumar <140904663+sunnykr7781@users.noreply.github.com>
2023-10-02 21:55:58 +0530

    Add files via upload

--- talaWebsiteClone/script.js
+++ talaWebsiteClone/script.js
@@ -16,7 +16,7 @@ icon.addEventListener("click", function () {
     icon.style.color = "#2b2b2b";
     flag = 1;
   } else {
-    menu.style.top = "-100%";
+    menu.style.top = "-110%";
     tala.style.color = "#dadada";
     cart.style.color = "#dadada";
     icon.style.color = "#dadada";
@@ -24,4 +24,44 @@ icon.addEventListener("click", function () {
 
     flag = 0;
   }
+});
+
+var t = gsap.timeline();
+t.from("#mainh h1", {
+  //   marginTop: 81,
+  x: 26,
+  y: -28,
+  //   rotate: -3,
+  scale: 0.86,
+  opacity: 0,
+  //   transition: all,
+  //   duration: 0.4,
+});
+t.from("#main h2 , #mainh h4", {
+  y: 15,
+  opacity: 0,
+  delay: -0.14,
+});
+t.to("#page1 img", {
+  scale: 1,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: "#page1 img",
+    // markers: true,
+    start: "top 85%",
+    end: "top 7%",
+    scrub: 3,
+  },
+});
+t.from("#page2 h1", {
+  rotateX: 90,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page2 h1",
+    scroller: "body",
+    // markers: true,
+    start: "top 72%",
+    end: "top 52%",
+    scrub: 3,
+  },
 });
